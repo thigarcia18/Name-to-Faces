cda3ac7b7085691171d595a17d083fe45633b8f2
Thiago Garcia <tgalves18@gmail.com>
2018-02-13 14:53:18 -0200

    First commit

---
+++ Name to Faces/Person.swift
@@ -0,0 +1,20 @@
+//
+//  Person.swift
+//  Name to Faces
+//
+//  Created by Thiago Garcia on 13/02/18.
+//  Copyright © 2018 iGarcia. All rights reserved.
+//
+
+import UIKit
+
+class Person: NSObject {
+
+    var name: String
+    var image: String
+    
+    init(name: String, image: String) {
+        self.name = name
+        self.image = image
+    }
+}
---
+++ Name to Faces/PersonCell.swift
@@ -0,0 +1,16 @@
+//
+//  PersonCell.swift
+//  Name to Faces
+//
+//  Created by Thiago Garcia on 13/02/18.
+//  Copyright © 2018 iGarcia. All rights reserved.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+    
+}
